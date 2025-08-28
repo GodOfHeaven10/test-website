5951a134b020ac880e07216c33443f739a8fb894
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-08-28 19:15:25 +0300

    s

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const body = document.body;
-const text = "Wow!"
+const oldText = document.querySelector(h1).textContent;
+const text = "Wow!";
 const textToChange = document.querySelector(h1);
 
-textToChange.addEventListener("hover", () => {
+//textToChange.addEventListener("mouseover", () => {
+//    textToChange.textContent = text
+//    document.head.appendChild(textToChange)
+//})
+textToChange.addEventListener("mouseenter", () => {
     textToChange.textContent = text
+    document.head.appendChild(textToChange)
+})
+
+textToChange.addEventListener("mouseleave", () => {
+    textToChange.textContent = oldText
     document.head.appendChild(textToChange)
 })
