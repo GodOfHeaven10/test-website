35c1fc87d8f4084241a889ca11b7f24d230585dd
karlioma100 <karlioma100@gmail.com>
2025-08-29 14:24:41 +0000

    s

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const body = document.body;
 const oldText = document.querySelector(h1).textContent;
 const text = "Wow!";
 const textToChange = document.querySelector(h1);
-
+//forget this under this, lets do more fun manipulation
 //textToChange.addEventListener("mouseover", () => {
 //    textToChange.textContent = text
 //    document.head.appendChild(textToChange)
@@ -16,3 +16,17 @@ textToChange.addEventListener("mouseleave", () => {
     textToChange.textContent = oldText
     document.head.appendChild(textToChange)
 })
+
+const buttons = ["Home", "About", "Contact", "Blog", "Games"]
+const incrementBy = 50
+
+for (i in buttons) {
+    const button = document.createElement("button")
+    button.setAttribute("id", buttons[i])
+    button.style.backgroundColor = hsl(0, 0, i*incrementBy)
+    button.style.height = 125
+    button.style.width = 25
+    button.style.alignSelf = center
+    button.textContent = buttons[i]
+    body.appendChild(button)
+}
