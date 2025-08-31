4c99afed6a16afdbecebe763bddbaf2c448110e8
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-08-31 15:56:26 +0300

    s

--- dom.js
+++ dom.js
@@ -3,7 +3,7 @@ const hoverTarget = document.getElementById("Hover-Test")
 hoverTarget.style.backgroundColor = oldColor
 
 var oldColor = "gray"
-hoverTarget.style.alignSelf = center
+//hoverTarget.style.alignSelf = center
 hoverTarget.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "red";
 })
