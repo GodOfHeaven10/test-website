9c84f539f7ce08f3e16b3759a6b84823d948ec62
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-08-31 17:47:32 +0300

    s

--- dom.js
+++ dom.js
@@ -13,3 +13,16 @@ hoverTarget.addEventListener("mouseout", (event) => {
 })
 
 const navigation = document.getElementsByClassName("Nav")
+
+navigation.getElementById("Triggers").addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "#555555ff"
+})
+
+navigation.getElementById("Triggers").addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor = "#1E1E1E"
+})
+
+navigation.getElementById("Triggers"). addEventListener('click'), (event) => {
+    event.target.style.backgroundColor = "#a3a3a3ff"
+    event.target.style.color = "#1E1E1E"
+}
