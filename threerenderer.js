8dfa177755f567c2bddd936ef0bd69557c01128c
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-09-04 20:12:30 +0300

    s

--- threerenderer.js
+++ threerenderer.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({scene, alpha: true});
+const renderer = new THREE.WebGLRenderer({scene, alpha: true, premultipliedAlpha: false});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
