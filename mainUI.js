d75136bee3543df04900c2d71c468b06712bea3f
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-10-14 21:11:52 +0300

    s

--- mainUI.js
+++ mainUI.js
@@ -82,15 +82,4 @@ window.addEventListener('resize' checkOrientation)
 document.addEventListener('DOMContentLoaded', checkOrientation)
 
 
-const numberOfTypes = 8
-
-//for (i in range(numberOfTypes)) {
-//    var labels = document.createElement("div")
-//    labels.style.width = "12.5%";
-//    labels.style.height = "100%";
-//    labels.style.backgroundColor = "transparent";
-//    bottomBarSelector.appendChild(labels)
-//}
-
-
 
